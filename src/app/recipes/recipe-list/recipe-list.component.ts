2ed0fba2df24f80f5e60bb374527df095263c531
Tom Biato <biatotom@gmail.com>
2024-03-13 17:19:27 +0100

    📝 (app.component.html): add top margin to the container div to improve spacing 📝 (app.module.ts): import NgOptimizedImage module to optimize image loading in the app 📝 (recipe-list.component.html): add a new recipe item template with name, description, and image 📝 (recipe-list.component.ts): create a Recipe model class and initialize a test recipe in the recipe list component

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Recipe } from "../recipe.model";
 
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrl: './recipe-list.component.css'
 })
-export class RecipeListComponent {
+export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('A Test Recipe', 'This is simply a test', 'https://www.foodandwine.com/thmb/fjNakOY7IcuvZac1hR3JcSo7vzI=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/FAW-recipes-pasta-sausage-basil-and-mustard-hero-06-cfd1c0a2989e474ea7e574a38182bbee.jpg'),
+  ];
 
+  constructor() {
+  }
+
+  ngOnInit() {
+  }
 }
